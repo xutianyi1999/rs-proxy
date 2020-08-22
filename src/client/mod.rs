8abbc0e27a1e4cc9e5ec57e381d4373c39391670
xty <853122726@qq.com>
2020-08-22 18:00:58 +0800

    update

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -19,11 +19,22 @@ pub async fn start() -> Result<()> {
   let cmc = Arc::new(ClientMuxChannel::new(tx));
   let channel_id = commons::create_channel_id();
 
-  connection_map.insert(channel_id, cmc.clone());
+  connection_map.insert(channel_id.clone(), cmc.clone());
   tokio::spawn(async move {
-    match cmc.recv_process(rx).await {
-      Ok(_) => {}
-      Err(e) => {}
+    let mut rx = rx;
+    let mut cmc = cmc;
+
+    loop {
+      match cmc.recv_process(rx).await {
+        Ok(_) => (),
+        Err(e) => {
+          eprintln!("{:?}", e);
+        }
+      };
+      connection_map.remove(&channel_id);
+      let (rx2, tx) = TcpStream::connect("127.0.0.1:12345").await?.into_split();
+      rx = rx2;
+      cmc = Arc::new(ClientMuxChannel::new(tx));
     }
   });
   Ok(())
